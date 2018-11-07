4389052d0adee7ab531fd6eaa6714a5fea17a194
morihirok <omnimani3@gmail.com>
2018-11-07 09:11:27 +0900

    finish p-41

--- small_step/statement.rb
+++ small_step/statement.rb
@@ -25,6 +25,24 @@ class If < Struct.new(:condition, :consequence, :alternative)
   end
 end
 
+class While < Struct.new(:condition, :body)
+  def to_s
+    "while (#{condition}) { #{body} }"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    [If.new(condition, Sequence.new(body, self), DoNothing.new), environment]
+  end
+end
+
 class DoNothing
   def to_s
     'do-nothing'
